e5370dce212c812f0abcf7789b8ec22eebaf555b
Rashid Bawah <rashbawah@gmail.com>
2023-03-11 10:59:43 -0700

    add first runnable model

--- models.py
+++ models.py
@@ -3,7 +3,14 @@ import torch.nn as nn
 from torchvision.models import inception_v3, Inception_V3_Weights
 
 
-def initialize_vision_model(model_name, num_classes, feature_extract, weights, use_pretrained=True):
+
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+
+
+def initialize_vision_model(model_name, num_classes, feature_extract, use_pretrained=True):
     model_ft = None
     input_size = 0
     
@@ -22,8 +29,10 @@ def initialize_vision_model(model_name, num_classes, feature_extract, weights, u
         num_ftrs = model_ft.fc.in_features
         model_ft.fc = nn.Linear(num_ftrs, num_classes)
         input_size = 299
-        print("Initializing InceptionV3 . . .")
-        
+        print("Initializing InceptionV3 with weights=Inception_V3_Weights.DEFAULT...")
+        print(f'Input size = {input_size}')
+        print(f'out_features = {num_classes}')
+
     else:
         print("Invalid Model Name!")
         exit()
